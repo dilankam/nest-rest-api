c292419a864a2786e5b826f4b0426261caf1fb3a
dilankatest <dilanka.test@gmail.com>
2019-06-16 10:47:18 +0530

    #POST #DTO - simple post router - DTO class

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-import { create } from 'domain';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
 
-@Controller('items') 
+@Controller('items')
 export class ItemsController {
     @Get() // http://localhost:3000/items
     findAll(): string {
         return 'Get all items';
+    }
+
+    @Post()
+    create(@Body() createItemDto: CreateItemDto): string {
+        return `Name: ${createItemDto.name} Desc ${createItemDto.description}`;
     }
 }
