237e354ed0c2c7f324f2308aa3ee4d3b09fcef4b
dilankatest <dilanka.test@gmail.com>
2019-06-17 12:07:10 +0530

    How to return response in express way

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body} from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
+import { Request, Response} from 'express'; //How to return response in express way
+import { Req, Res } from '@nestjs/common'; //How to return response in express way
 
 @Controller('items')
 export class ItemsController {
+    //How to return response in express way
     @Get() // http://localhost:3000/items
-    findAll(): string {
-        return 'Get all items';
+    findAll(@Req() req: Request, @Res() res: Response): Response {
+        console.log(req.url);
+        return res.send("sfsdf sdf");
     }
+
+    // @Get() // http://localhost:3000/items
+    // findAll(): string {
+    //     return 'Get all items';
+    // }
 
     @Post()
     create(@Body() createItemDto: CreateItemDto): string {
