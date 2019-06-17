148134bd1d95639431bd9add6774808c8e2c214b
dilankatest <dilanka.test@gmail.com>
2019-06-17 16:32:35 +0530

    get request with url parameter

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,21 +1,37 @@
-import { Controller, Get, Post, Body} from '@nestjs/common';
+import { Controller, Get, Post, Body,Param} from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
-import { Request, Response} from 'express'; //How to return response in express way
-import { Req, Res } from '@nestjs/common'; //How to return response in express way
+// import { Request, Response} from 'express'; //How to return response in express way
+// import { Req, Res } from '@nestjs/common'; //How to return response in express way
 
 @Controller('items')
 export class ItemsController {
+    /*
     //How to return response in express way
     @Get() // http://localhost:3000/items
     findAll(@Req() req: Request, @Res() res: Response): Response {
         console.log(req.url);
         return res.send("sfsdf sdf");
     }
+    */
 
-    // @Get() // http://localhost:3000/items
-    // findAll(): string {
-    //     return 'Get all items';
-    // }
+    @Get() // http://localhost:3000/items
+    findAll(): string {
+        return 'Get all items';
+    }
+
+    /*
+    // with get parameter
+    @Get(':id')
+    findOne(@Param() param): string {
+        return `Id : ${param.id}`;
+    }
+    */
+
+    // with get parameter
+   @Get(':id')
+   findOne(@Param('id') id): string {
+       return `Id : ${id}`;
+   }
 
     @Post()
     create(@Body() createItemDto: CreateItemDto): string {
